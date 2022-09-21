1a5d72d7e1c9f9a5a702455bc3c99f45f64a1ea3
Ysedda <arthur.baudoin@icloud.com>
2022-09-21 16:36:52 +0200

    added alt to img tag

--- src/components/RobotCard.jsx
+++ src/components/RobotCard.jsx
@@ -12,7 +12,7 @@ const RobotCard = (robot) => {
         <div className="cards">
         <div className="card">
             <h2>{robot.name}</h2>
-            <img src = {`https://robohash.org/${robot.id}?set=set2`} />
+            <img src = {`https://robohash.org/${robot.id}?set=set2`} alt="{robot.name}"/>
             <p>Username: {robot.username}</p>
             <p>Phone: {robot.phone}</p>
             <p>Email: {robot.email}</p>
